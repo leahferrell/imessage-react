015f3c3da0c9ba7d45cb1f3a5f11128ac7469fc7
Leah Ferrell <leahferrell03@gmail.com>
2019-10-13 04:03:27 -0500

    minor css tweaks

--- src/components/phone/messages/message/index.js
+++ src/components/phone/messages/message/index.js
@@ -6,7 +6,7 @@ const Message = ({ isMe, message }) => {
   let className = (isMe ? "mine": "yours");
 
   return (
-    <div className={[styles[className], styles["messages"], "animated", "slideInUp", "delay-5s", "slow"].join(' ')}>
+    <div className={[styles[className], styles["messages"], "animated", "slideInUp", "delay-4s", "slow"].join(' ')}>
       <p
         className={styles["message"]}
       >
